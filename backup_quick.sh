b43cf4b4524ae9715260caf086c4837f40b3f635
Patrick Knöbel <5472845+im-pro-at@users.noreply.github.com>
2021-01-04 17:24:33 +0100

    Update backup_quick.sh

--- backup_quick.sh
+++ backup_quick.sh
@@ -5,6 +5,7 @@ echo "Report will be sent to $GMVAULT_SEND_REPORTS_TO."
 
 gmvault sync -t quick -d /data $GMVAULT_OPTIONS $GMVAULT_EMAIL_ADDRESS --passwd 2>&1 \
 	| tee /data/${GMVAULT_EMAIL_ADDRESS}_quick.log \
+	| tail \
 	| mail -s "Mail Backup (quick) | $GMVAULT_EMAIL_ADDRESS | `date +'%Y-%m-%d %r %Z'`" $GMVAULT_SEND_REPORTS_TO
 
 echo "Quick sync complete."
