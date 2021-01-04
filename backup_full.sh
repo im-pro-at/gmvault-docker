fc1fe322b280f560e8d132e895b1219c7010ecf4
Patrick Knöbel <5472845+im-pro-at@users.noreply.github.com>
2021-01-04 17:24:08 +0100

    Update backup_full.sh

--- backup_full.sh
+++ backup_full.sh
@@ -5,6 +5,7 @@ echo "Report will be sent to $GMVAULT_SEND_REPORTS_TO."
 
 gmvault sync -d /data $GMVAULT_OPTIONS $GMVAULT_EMAIL_ADDRESS --passwd 2>&1 \
 	| tee /data/${GMVAULT_EMAIL_ADDRESS}_full.log \
+	| tail \
 	| mail -s "Mail Backup (full) | $GMVAULT_EMAIL_ADDRESS | `date +'%Y-%m-%d %r %Z'`" $GMVAULT_SEND_REPORTS_TO
 
 echo "Full sync complete."
